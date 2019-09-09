28f1cce91784b46a3ac9c4cbbf36d9fa4eefe7a9
Kyle Koder <silvertallon@gmail.com>
2019-09-09 09:37:37 -0700

    Initial commit

---
+++ src/main/java/com/fantasticsource/rpgquesting/RPGQuesting.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.rpgquesting;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = RPGQuesting.MODID, name = RPGQuesting.NAME, version = RPGQuesting.VERSION)
+public class RPGQuesting
+{
+    public static final String MODID = "rpgquesting";
+    public static final String NAME = "RPG Questing";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(RPGQuesting.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
