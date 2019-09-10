2ee9ae52ba228ca730e8bdd61dad6470fe46e930
Kyle Koder <silvertallon@gmail.com>
2019-09-09 17:11:32 -0700

    TODO and lib changes, and some minor stuff in Dialogue and Quest

--- src/main/java/com/fantasticsource/rpgquesting/dialogue/Dialogue.java
+++ src/main/java/com/fantasticsource/rpgquesting/dialogue/Dialogue.java
@@ -1,14 +1,16 @@
 package com.fantasticsource.rpgquesting.dialogue;
 
+import com.fantasticsource.mctools.component.CStringUTF8;
+import com.fantasticsource.mctools.component.Component;
 import net.minecraft.entity.Entity;
 
-public class Dialogue
+public class Dialogue extends Component
 {
-    public final String name;
+    public final CStringUTF8 name;
 
     public Dialogue(String name)
     {
-        this.name = name;
+        this.name = new CStringUTF8(this);
     }
 
     public boolean entityHas(Entity entity)
--- src/main/java/com/fantasticsource/rpgquesting/quest/Quest.java
+++ src/main/java/com/fantasticsource/rpgquesting/quest/Quest.java
@@ -1,5 +1,50 @@
 package com.fantasticsource.rpgquesting.quest;
 
-public class Quest
+import com.fantasticsource.mctools.component.CStringUTF8;
+import com.fantasticsource.mctools.component.Component;
+import io.netty.buffer.ByteBuf;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Quest extends Component
 {
+    public CStringUTF8 name, description;
+
+    @Override
+    public void write(ByteBuf byteBuf)
+    {
+
+    }
+
+    @Override
+    public void read(ByteBuf byteBuf)
+    {
+
+    }
+
+    @Override
+    public void save(FileOutputStream fileOutputStream) throws IOException
+    {
+
+    }
+
+    @Override
+    public void load(FileInputStream fileInputStream) throws IOException
+    {
+
+    }
+
+    @Override
+    public void parse(String s)
+    {
+
+    }
+
+    @Override
+    public Component copy()
+    {
+        return null;
+    }
 }
