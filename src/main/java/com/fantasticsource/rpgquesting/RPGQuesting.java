dfc95b1460ecbf936eec92d6270f557179befc48
Kyle Koder <silvertallon@gmail.com>
2019-09-19 00:18:38 -0700

    TODO update, comment out debug quest addition so it doesn't duplicate

--- src/main/java/com/fantasticsource/rpgquesting/RPGQuesting.java
+++ src/main/java/com/fantasticsource/rpgquesting/RPGQuesting.java
@@ -1,20 +1,11 @@
 package com.fantasticsource.rpgquesting;
 
 import com.fantasticsource.mctools.MCTools;
-import com.fantasticsource.rpgquesting.actions.*;
-import com.fantasticsource.rpgquesting.conditions.*;
-import com.fantasticsource.rpgquesting.dialogue.CDialogue;
-import com.fantasticsource.rpgquesting.dialogue.CDialogueBranch;
-import com.fantasticsource.rpgquesting.dialogue.CDialogueChoice;
 import com.fantasticsource.rpgquesting.dialogue.CDialogues;
-import com.fantasticsource.rpgquesting.quest.CQuest;
 import com.fantasticsource.rpgquesting.quest.CQuests;
 import com.fantasticsource.rpgquesting.quest.JournalGUI;
 import com.fantasticsource.rpgquesting.quest.QuestTracker;
-import com.fantasticsource.rpgquesting.quest.objective.CObjectiveCollect;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
@@ -139,54 +130,54 @@ public class RPGQuesting
             //..........................................
 
 
-            String name = "The Wolves are Hungry";
-            CCondition[] dialogueConditions = new CCondition[]{new CConditionEntityEntryIs("wolf")};
-            ItemStack toCollect = new ItemStack(Items.CHICKEN, 5);
-
-
-            CQuest quest = new CQuest(name, "The Wolves", 1, false);
-            CQuests.add(quest);
-
-            quest.addObjectives(new CObjectiveCollect("chicken meat collected", toCollect));
-            quest.setExp(15);
-
-
-            CDialogue dialogue = new CDialogue().setName(name + " (quest)");
-            CDialogues.add(dialogue);
-
-            dialogue.addPlayerConditions(new CConditionQuestAvailable(quest));
-            dialogue.addEntityConditions(dialogueConditions);
-
-            CDialogueBranch branch = new CDialogueBranch("The pack hungers, two-legs...we hunger for the meat of the winged ones.");
-            dialogue.add(branch);
-            branch.add(new CDialogueChoice("Then I hunt").setAction(new CActionArray(new CActionStartQuest(quest), new CActionEndDialogue())));
-            branch.add(new CDialogueChoice("You'll have to fend for yourselves").setAction(new CActionEndDialogue()));
-
-
-            dialogue = new CDialogue().setName(name + " (in progress)");
-            CDialogues.add(dialogue);
-
-            dialogue.addPlayerConditions(new CConditionQuestInProgress(quest));
-            dialogue.addEntityConditions(dialogueConditions);
-
-            branch = new CDialogueBranch("I appreciate the effort, two-legs, but I fear this amount is not enough; the pack would surely fight over it unless we have more.");
-            dialogue.add(branch);
-            branch.add(new CDialogueChoice("End Dialogue").setAction(new CActionEndDialogue()));
-
-
-            dialogue = new CDialogue().setName(name + " (complete)");
-            CDialogues.add(dialogue);
-
-            dialogue.addPlayerConditions(new CConditionQuestReadyToComplete(quest));
-            dialogue.addEntityConditions(dialogueConditions);
-
-            branch = new CDialogueBranch("Thank you, two-legs.  I share with you some of my knowledge of the forest");
-            dialogue.add(branch);
-
-            CDialogueChoice choice = new CDialogueChoice("I see");
-            branch.add(choice);
-            choice.setAction(new CActionArray(new CActionTakeItems(toCollect).addConditions(new CConditionHaveItems(toCollect)), new CActionEndDialogue(), new CActionCompleteQuest(quest)));
-            branch.add(new CDialogueChoice("End Dialogue").setAction(new CActionEndDialogue()));
+//            String name = "The Wolves are Hungry";
+//            CCondition[] dialogueConditions = new CCondition[]{new CConditionEntityEntryIs("wolf")};
+//            ItemStack toCollect = new ItemStack(Items.CHICKEN, 5);
+//
+//
+//            CQuest quest = new CQuest(name, "The Wolves", 1, false);
+//            CQuests.add(quest);
+//
+//            quest.addObjectives(new CObjectiveCollect("chicken meat collected", toCollect));
+//            quest.setExp(15);
+//
+//
+//            CDialogue dialogue = new CDialogue().setName(name + " (quest)");
+//            CDialogues.add(dialogue);
+//
+//            dialogue.addPlayerConditions(new CConditionQuestAvailable(quest));
+//            dialogue.addEntityConditions(dialogueConditions);
+//
+//            CDialogueBranch branch = new CDialogueBranch("The pack hungers, two-legs...we hunger for the meat of the winged ones.");
+//            dialogue.add(branch);
+//            branch.add(new CDialogueChoice("Then I hunt").setAction(new CActionArray(new CActionStartQuest(quest), new CActionEndDialogue())));
+//            branch.add(new CDialogueChoice("You'll have to fend for yourselves").setAction(new CActionEndDialogue()));
+//
+//
+//            dialogue = new CDialogue().setName(name + " (in progress)");
+//            CDialogues.add(dialogue);
+//
+//            dialogue.addPlayerConditions(new CConditionQuestInProgress(quest));
+//            dialogue.addEntityConditions(dialogueConditions);
+//
+//            branch = new CDialogueBranch("I appreciate the effort, two-legs, but I fear this amount is not enough; the pack would surely fight over it unless we have more.");
+//            dialogue.add(branch);
+//            branch.add(new CDialogueChoice("End Dialogue").setAction(new CActionEndDialogue()));
+//
+//
+//            dialogue = new CDialogue().setName(name + " (complete)");
+//            CDialogues.add(dialogue);
+//
+//            dialogue.addPlayerConditions(new CConditionQuestReadyToComplete(quest));
+//            dialogue.addEntityConditions(dialogueConditions);
+//
+//            branch = new CDialogueBranch("Thank you, two-legs.  I share with you some of my knowledge of the forest");
+//            dialogue.add(branch);
+//
+//            CDialogueChoice choice = new CDialogueChoice("I see");
+//            branch.add(choice);
+//            choice.setAction(new CActionArray(new CActionTakeItems(toCollect).addConditions(new CConditionHaveItems(toCollect)), new CActionEndDialogue(), new CActionCompleteQuest(quest)));
+//            branch.add(new CDialogueChoice("End Dialogue").setAction(new CActionEndDialogue()));
         }
     }
 
