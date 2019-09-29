1ae7e66404a0dc8db3f8cd622901615fdf98684d
Kyle Koder <silvertallon@gmail.com>
2019-09-28 17:00:20 -0700

    Selecting an element in the item selector now sets the entry in the quest rewards

--- src/main/java/com/fantasticsource/rpgquesting/ItemSelectionGUI.java
+++ src/main/java/com/fantasticsource/rpgquesting/ItemSelectionGUI.java
@@ -1,6 +1,8 @@
 package com.fantasticsource.rpgquesting;
 
+import com.fantasticsource.mctools.gui.GUILeftClickEvent;
 import com.fantasticsource.mctools.gui.GUIScreen;
+import com.fantasticsource.mctools.gui.element.GUIElement;
 import com.fantasticsource.mctools.gui.element.other.GUIVerticalScrollbar;
 import com.fantasticsource.mctools.gui.element.text.GUIItemStack;
 import com.fantasticsource.mctools.gui.element.text.GUIText;
@@ -8,31 +10,42 @@ import com.fantasticsource.mctools.gui.element.view.GUIScrollView;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemSelectionGUI extends GUIScreen
 {
-    private GUIScrollView scrollView;
-    private GUIItemStack clickedElement;
+    public static ItemSelectionGUI GUI;
+
+    private static GUIScrollView scrollView;
+    private static GUIItemStack clickedElement;
+
+    static
+    {
+        GUI = new ItemSelectionGUI();
+        MinecraftForge.EVENT_BUS.register(ItemSelectionGUI.class);
+    }
 
     public static void show(GUIItemStack clickedElement)
     {
-        ItemSelectionGUI gui = new ItemSelectionGUI();
+        if (Minecraft.getMinecraft().currentScreen instanceof GUIScreen) GUIScreen.showStacked(GUI);
+        else Minecraft.getMinecraft().displayGuiScreen(GUI);
 
-        if (Minecraft.getMinecraft().currentScreen instanceof GUIScreen) GUIScreen.showStacked(gui);
-        else Minecraft.getMinecraft().displayGuiScreen(gui);
+        ItemSelectionGUI.clickedElement = clickedElement;
 
-        gui.clickedElement = clickedElement;
 
-        gui.scrollView.add(new GUIText(gui, "\n"));
+        scrollView.clear();
 
-        gui.scrollView.add(new GUIItemStack(gui, clickedElement.stack.copy()));
-        gui.scrollView.add(new GUIText(gui, "\n"));
+        scrollView.add(new GUIText(GUI, "\n"));
+
+        scrollView.add(new GUIItemStack(GUI, clickedElement.getStack().copy()));
+        scrollView.add(new GUIText(GUI, "\n"));
 
         EntityPlayer player = Minecraft.getMinecraft().player;
         for (int i = 0; i < player.inventory.getSizeInventory(); i++)
         {
-            gui.scrollView.add(new GUIItemStack(gui, player.inventory.getStackInSlot(i).copy()));
-            gui.scrollView.add(new GUIText(gui, "\n"));
+            scrollView.add(new GUIItemStack(GUI, player.inventory.getStackInSlot(i).copy()));
+            scrollView.add(new GUIText(GUI, "\n"));
         }
     }
 
@@ -44,5 +57,18 @@ public class ItemSelectionGUI extends GUIScreen
         scrollView = new GUIScrollView(this, 0.02, 0, 0.94, 1);
         guiElements.add(scrollView);
         guiElements.add(new GUIVerticalScrollbar(this, 0.98, 0, 0.02, 1, Color.GRAY, Color.BLANK, Color.WHITE, Color.BLANK, scrollView));
+    }
+
+    @SubscribeEvent
+    public static void click(GUILeftClickEvent event)
+    {
+        if (Minecraft.getMinecraft().currentScreen != GUI) return;
+
+        GUIElement element = event.getElement();
+        if (element instanceof GUIItemStack)
+        {
+            clickedElement.setStack(((GUIItemStack) element).getStack().copy());
+            GUI.close();
+        }
     }
 }
