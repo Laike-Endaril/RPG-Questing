a817db7527560147ccd1485e39a9f8abe4592dc8
Kyle Koder <silvertallon@gmail.com>
2019-09-30 15:21:14 -0700

    Condition editor is working for name condition, but condition editing scrollview is messed up

--- src/main/java/com/fantasticsource/rpgquesting/conditions/gui/ConditionEditorGUI.java
+++ src/main/java/com/fantasticsource/rpgquesting/conditions/gui/ConditionEditorGUI.java
@@ -5,8 +5,10 @@ import com.fantasticsource.mctools.gui.element.GUIElement;
 import com.fantasticsource.mctools.gui.element.other.GUIGradient;
 import com.fantasticsource.mctools.gui.element.other.GUIGradientBorder;
 import com.fantasticsource.mctools.gui.element.other.GUIVerticalScrollbar;
+import com.fantasticsource.mctools.gui.element.text.GUILabeledTextInput;
 import com.fantasticsource.mctools.gui.element.text.GUIText;
 import com.fantasticsource.mctools.gui.element.text.GUITextButton;
+import com.fantasticsource.mctools.gui.element.text.filter.FilterNotEmpty;
 import com.fantasticsource.mctools.gui.element.view.GUIScrollView;
 import com.fantasticsource.rpgquesting.conditions.*;
 import com.fantasticsource.rpgquesting.conditions.quest.CConditionQuestAvailable;
@@ -23,6 +25,7 @@ import static com.fantasticsource.rpgquesting.quest.JournalGUI.RED;
 public class ConditionEditorGUI extends GUIScreen
 {
     public CCondition original, selection;
+    public GUICondition current;
     private GUIGradientBorder separator;
     private GUIScrollView conditionSelector, conditionEditor;
     private GUIVerticalScrollbar conditionSelectorScroll, conditionEditorScroll;
@@ -45,7 +48,7 @@ public class ConditionEditorGUI extends GUIScreen
 
 
         //Management
-        GUICondition current = new GUICondition(this, original);
+        current = new GUICondition(this, original);
         GUITextButton save = new GUITextButton(this, "Save and Close", JournalGUI.GREEN[0]);
         root.add(save.addClickActions(() ->
         {
@@ -73,7 +76,7 @@ public class ConditionEditorGUI extends GUIScreen
 
         root.add(new GUIText(this, TextFormatting.GOLD + "Original: "));
         GUICondition originalElement = new GUICondition(this, current.condition == null ? null : (CCondition) current.condition.copy());
-        root.add(originalElement.addClickActions(() -> current.setCondition(originalElement.condition)));
+        root.add(originalElement.addClickActions(() -> setCurrent(originalElement.condition)));
         root.add(new GUIGradientBorder(this, 1, 0.01, 0.3, Color.GRAY, Color.GRAY.copy().setAF(0.3f)));
 
         root.add(new GUIText(this, TextFormatting.GOLD + "Current: "));
@@ -140,7 +143,7 @@ public class ConditionEditorGUI extends GUIScreen
             GUIElement element = conditionSelector.get(i);
             if (element instanceof GUICondition)
             {
-                element.addClickActions(() -> current.setCondition(((GUICondition) element).condition));
+                element.addClickActions(() -> setCurrent(((GUICondition) element).condition));
             }
         }
 
@@ -151,6 +154,8 @@ public class ConditionEditorGUI extends GUIScreen
 
         conditionEditorScroll = new GUIVerticalScrollbar(this, 0.98, yy2, 0.02, 1 - yy2, Color.GRAY, Color.BLANK, Color.WHITE, Color.BLANK, conditionEditor);
         root.add(conditionEditorScroll);
+
+        setCurrent(current.condition);
     }
 
     @Override
@@ -180,5 +185,25 @@ public class ConditionEditorGUI extends GUIScreen
     @Override
     protected void init()
     {
+    }
+
+    private void setCurrent(CCondition condition)
+    {
+        current.setCondition(condition);
+
+
+        conditionEditor.clear();
+
+        Class cls = condition.getClass();
+        if (cls == CConditionNameIs.class)
+        {
+            GUILabeledTextInput name = new GUILabeledTextInput(this, "Name: ", ((CConditionNameIs) condition).name.value, FilterNotEmpty.INSTANCE);
+            name.input.addRecalcActions(() ->
+            {
+                ((CConditionNameIs) condition).name.set(name.input.text);
+                current.setCondition(condition);
+            });
+            conditionEditor.add(name);
+        }
     }
 }
